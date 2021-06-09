eff3db1c2479615e2812e2cec1db407cefb608eb
Vladislav <vladrjeousski@gmail.com>
2021-06-09 14:28:23 +0300

    feat: added additional signup link

--- src/Components/Body/SignUpSection/index.js
+++ src/Components/Body/SignUpSection/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import'./index.css';
 
 function SignUpSection() {
     return (
         <div className='body-container'>
             <h2 className='container-title'>Ready to start?</h2>
-            <button type='submit' className='container-signup'>Sign Up</button>
+            <Link to='/signup'><button type='submit' className='container-signup'>Sign Up</button></Link>
         </div>
     );
   }
