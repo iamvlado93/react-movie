818b96aacad8125a59bc51162ddcfc47917ce3a7
Vladislav <vladrjeousski@gmail.com>
2021-06-22 16:54:02 +0300

    feat: fixed styles for all components

--- src/Components/Body/TeleSection/index.js
+++ src/Components/Body/TeleSection/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-import tv from '../../Images/tv.png';
+import tv from '../../../Images/tv.png';
 
-import'./index.css';
+import './index.css';
 
 function TeleSection() {
-    return (
-      <div className="section-tv">
-          <div className='tv-container'>
-              <div className='container-left'>
-                  <img src={tv} alt='tv'></img>
-              </div>
-              <div className='container-right'>
-                  <p>Watch on Smart TV, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray and etc.</p>
-              </div>
-          </div>
+  return (
+    <div className="section-tv">
+      <div className="tv-container">
+        <div className="container-left">
+          <img src={tv} alt="tv"></img>
+        </div>
+        <div className="container-right">
+          <p>Watch on Smart TV, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray and etc.</p>
+        </div>
       </div>
-    );
-  }
-  
-  export default TeleSection;
+    </div>
+  );
+}
+
+export default TeleSection;
