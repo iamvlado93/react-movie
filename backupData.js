74c744c4744b6518852371cba905b9d413ca4685
Vladislav <vladrjeousski@gmail.com>
2021-06-30 17:59:28 +0300

    feat: added movie data backup

---
+++ backupData.js
@@ -0,0 +1,163 @@
+// import mov1 from '../../Images/mov1.jpg';
+// import mov2 from '../../Images/mov2.jpg';
+// import mov3 from '../../Images/mov3.jpg';
+// import mov4 from '../../Images/mov4.jpg';
+// import mov5 from '../../Images/mov5.jpg';
+// import mov6 from '../../Images/mov6.jpg';
+// import mov7 from '../../Images/mov7.jpg';
+// import mov8 from '../../Images/mov8.jpg';
+// import mov9 from '../../Images/mov9.jpg';
+// import mov10 from '../../Images/mov10.jpg';
+// import mov11 from '../../Images/mov11.png';
+// import mov12 from '../../Images/mov12.jpg';
+
+// const data = {
+//   movies: [
+//     {
+//       id: 1,
+//       name: 'Black Widow',
+//       image: mov1,
+//       description:
+//         'Natasha Romanoff aka Black Widow confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by a force that will stop at nothing to bring her down, Natasha must deal with her history as a spy and the broken relationships left in her wake long before she became an Avenger.',
+//       country: 'USA',
+//       year: '2015',
+//       genre: 'Fantasy',
+//       duration: '1h 23m',
+//       rating: '8.6',
+//     },
+//     {
+//       id: 2,
+//       name: 'Replicas',
+//       image: mov2,
+//       description:
+//         'Replicas is a 2018 American science fiction thriller film directed by Jeffrey Nachmanoff, and written by Chad St. John, from a story by Stephen Hamel. The film tells the story of a neuroscientist who violates the law and bioethics to bring his family members back to life after they die in a car accident.',
+//       country: 'USA',
+//       year: '2020',
+//       genre: 'Action',
+//       duration: '1h 47m',
+//       rating: '7.3',
+//     },
+//     {
+//       id: 3,
+//       name: 'Us',
+//       image: mov3,
+//       description:
+//         'Us is a 2019 American horror film written and directed by Jordan Peele, starring Lupita Nyongo, Winston Duke, Elisabeth Moss, and Tim Heidecker. The film follows Adelaide Wilson (Nyongo) and her family, who are attacked by a group of menacing doppelgangers.',
+//       country: 'USA',
+//       year: '2019',
+//       genre: 'Action',
+//       duration: '1h 32m',
+//       rating: '9.0',
+//     },
+//     {
+//       id: 4,
+//       name: 'Archer',
+//       image: mov4,
+//       description:
+//         'Lauren Pierce has just become the high-school Tri-State Archery Champion. After the competition, Lauren and her teammate Emily return to their hotel room for a night of irresponsible celebratory drinking that grows into more. When interrupted by Emilys abusive boyfriend, Lauren snaps and brutally beats Daniel. Lauren is then sentenced to a girls reform camp Paradise Ridge, nestled in the mountains of California. But this "reform camp" turns out to be a corrupt and twisted prison that breaks young girls and keeps parents in the dark. With the help of Rebecca, a strong and provocative young woman whom Lauren befriends, the two escape the unsafe facility and fight for their lives out in the Utah wilderness; a journey of growth, acceptance and resilience of what they believe is right.',
+//       country: 'USA',
+//       year: '2016',
+//       genre: 'Adventure',
+//       duration: '1h 22m',
+//       rating: '7.8',
+//     },
+//     {
+//       id: 5,
+//       name: 'Star Wars',
+//       image: mov5,
+//       description:
+//         'The Last Jedi follows Rey as she seeks the aid of Luke Skywalker, in hopes of turning the tide for the Resistance in the fight against Kylo Ren and the First Order, while General Leia Organa, Finn, and Poe Dameron attempt to escape a First Order attack on the dwindling Resistance fleet.',
+//       country: 'USA',
+//       year: '2017',
+//       genre: 'Fantasy',
+//       duration: '1h 46m',
+//       rating: '6.2',
+//     },
+//     {
+//       id: 6,
+//       name: 'Joker',
+//       image: mov6,
+//       description:
+//         'Set in 1981, it follows Arthur Fleck, a failed clown and stand-up comedian whose descent into insanity and nihilism inspires a violent counter-cultural revolution against the wealthy in a decaying Gotham City.',
+//       country: 'USA',
+//       year: '2019',
+//       genre: 'Drama',
+//       duration: '1h 33m',
+//       rating: '8.5',
+//     },
+//     {
+//       id: 7,
+//       name: 'The Man Without Gravity',
+//       image: mov7,
+//       description:
+//         'On a stormy night at a hospital in a small town, a child is born with an unimaginable power: he can resist gravity. The newborn baby floats in the air, hovering in the room, as light as a feather, watched by his incredulous mother. The mother and grandmother soon flee with the newborn and decide to keep him hidden from the world for many, many years. Until one day, the world finds out about the man without gravity...',
+//       country: 'Belgium',
+//       year: '2019',
+//       genre: 'Comedy',
+//       duration: '1h 07m',
+//       rating: '4.0',
+//     },
+//     {
+//       id: 8,
+//       name: 'Wrath of Man',
+//       image: mov8,
+//       description:
+//         'The plot follows H, a cold and mysterious character working at a cash truck company responsible for moving hundreds of millions of dollars around Los Angeles each week.',
+//       country: 'USA',
+//       year: '2021',
+//       genre: 'Action',
+//       duration: '2h 01m',
+//       rating: '9.4',
+//     },
+//     {
+//       id: 9,
+//       name: 'Father',
+//       image: mov9,
+//       description:
+//         'The Father is a movie about dementia, which presents it from the perspective of a person suffering from dementia. Based on a by the director, Florian Zeller, The Father maintains that same essence of watching one, with most of the frames being still and the atmosphere which is bright yet uncomfortably eerie.',
+//       country: 'UK',
+//       year: '2020',
+//       genre: 'Biography',
+//       duration: '1h 48m',
+//       rating: '4.0',
+//     },
+//     {
+//       id: 10,
+//       name: 'Paris Police 1900',
+//       image: mov10,
+//       description:
+//         'The corpse of an unknown woman is found in the river Seine. The investigation will push a young ambitious inspector to discover a heavy state secret.',
+//       country: 'France',
+//       year: '2021',
+//       genre: 'History',
+//       duration: '30m',
+//       rating: '8.1',
+//     },
+//     {
+//       id: 11,
+//       name: 'Cherry',
+//       image: mov11,
+//       description:
+//         'When Cherry returns home a war hero, he battles the demons of undiagnosed PTSD and spirals into drug addiction, surrounding himself with a menagerie of depraved misfits. Draining his finances, Cherry turns to bank robbing to fund his addiction, shattering his relationship with Emily along the way.',
+//       country: 'UK',
+//       year: '2021',
+//       genre: 'Drama',
+//       duration: '1h 29m',
+//       rating: '8.4',
+//     },
+//     {
+//       id: 12,
+//       name: 'Dont Breathe',
+//       image: mov12,
+//       description:
+//         'The film stars Jane Levy, Dylan Minnette, Daniel Zovatto, and Stephen Lang, and focuses on three friends who get trapped inside a blind mans house while breaking into it. The film was produced by Ghost House Pictures and Good Universe and distributed by Sony Pictures Releasing.',
+//       country: 'UK',
+//       year: '2016',
+//       genre: 'Scary',
+//       duration: '1h 59m',
+//       rating: '8.9',
+//     },
+//   ],
+// };
+
+// export default data;
